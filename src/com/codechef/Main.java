a6a7d0d60aa8f1b605b303019f10ba79b51260b8
Nitin <nsinghboss02@gmail.com>
2021-12-05 11:28:17 +0530

    Initial commit

---
+++ src/com/codechef/Main.java
@@ -0,0 +1,29 @@
+package com.codechef;
+
+public class Main {
+
+    public static void main(String[] args) {
+	/*
+       * * * * *
+       *       *
+       *       *
+       *       *
+       * * * * *
+	 */
+
+for(int i=1;i<=5;i++){
+    for(int k=1;k<=5;k++){
+        System.out.print(" ");
+    }
+    for(int j=1;j<=5;j++){
+        if(i==1 || i==5 || j==1||j==5){
+            System.out.print("*");
+        }
+        else{
+            System.out.print(" ");
+        }
+    }
+    System.out.println();
+}
+    }
+}
